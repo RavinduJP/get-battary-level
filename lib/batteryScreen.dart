8df4d772c233968263bd6eaf9b4410a258827e65
Ravindu Thakshila Jayasooriya <62090384+RavinduJP@users.noreply.github.com>
2025-08-04 12:53:37 +0530

    create simple UI

---
+++ lib/batteryScreen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class BatteryScreen extends StatefulWidget {
+  const BatteryScreen({super.key});
+
+  @override
+  State<BatteryScreen> createState() => _BatteryScreenState();
+}
+
+class _BatteryScreenState extends State<BatteryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: 500,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Waiting ....',
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.blue.shade900,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 10),
+                ),
+                child: const Text(
+                  'Get Battery Level',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
