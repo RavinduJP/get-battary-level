1af2ab70da16b72cf3c1f3535f02f822e3b455b9
Ravindu Thakshila Jayasooriya <62090384+RavinduJP@users.noreply.github.com>
2025-08-05 12:04:45 +0530

    write get battery level method

--- lib/batteryScreen.dart
+++ lib/batteryScreen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: sort_child_properties_last
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class BatteryScreen extends StatefulWidget {
   const BatteryScreen({super.key});
@@ -8,23 +11,26 @@ class BatteryScreen extends StatefulWidget {
 }
 
 class _BatteryScreenState extends State<BatteryScreen> {
+  static const batteryChannel = MethodChannel('battery');
+
+  String _batteryLevel = 'Waiting ...';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  Container(
+      body: Container(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                'Waiting ....',
+                _batteryLevel,
                 style: TextStyle(
                   fontSize: 15.0,
                   color: Colors.blue.shade900,
                 ),
               ),
               ElevatedButton(
-                onPressed: () {},
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.black,
                   foregroundColor: Colors.white,
@@ -34,11 +40,33 @@ class _BatteryScreenState extends State<BatteryScreen> {
                   'Get Battery Level',
                   style: TextStyle(color: Colors.white),
                 ),
+                onPressed: _getBatteryLevel,
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  Future _getBatteryLevel() async {
+    String batteryLevel;
+    // final int newBatteryLevel =
+    //     await batteryChannel.invokeMethod('getBatteryLevel');
+
+    // setState(() {
+    //   batteryLevel = '$newBatteryLevel';
+    // });
+
+    try {
+      final result = await batteryChannel.invokeMethod<int>('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
   }
 }
